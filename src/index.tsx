425f8b1065221e698791abfb2c42487b7aacbc58
Matheusmottaa <matheusfelipe1918@gmail.com>
2022-04-13 20:16:54 -0300

    simulate connection with mirage js

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import {render} from 'react-dom';
-import {createServer} from "miragejs";
+import {createServer, Model} from "miragejs";
 import {App} from './App';
 
 createServer({ 
+
+  models:{ 
+    transaction: Model, 
+  },
+
+  seeds(server){ 
+    server.db.loadData({
+    })
+  },
+
   routes(){ 
     this.namespace = 'api'; 
 
     this.get('/transactions', ()=>{ 
-      return [
-        {
-          id: 1,
-          title: "Transaction 1",
-          amount: 500,
-          type: 'Deposit',
-          category: 'Food', 
-          createdAt: new Date()
-        }
-      ]
+      return this.schema.all('transaction')
     })
+
+    this.post('/transactions', (schema, request)=>{
+      const data = JSON.parse(request.requestBody); 
+      return schema.create('transaction', data); 
+    }); 
   }
 })
 
